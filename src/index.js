d4e9e7f533021e2e63950912dc89fffdc3c7b56d
timand1 <80768929+timand1@users.noreply.github.com>
2022-07-14 12:24:17 +0200

    1

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename='/shopperlist'>
         <App />
       </BrowserRouter>
     </React.StrictMode>
